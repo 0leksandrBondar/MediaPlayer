2b84504a36938483f98ebd1b5f46a421e5fd07ff
b1tfly <thisisandrew0110@gmail.com>
2023-11-15 02:57:44 -1200

    added basic main window, corrected CMakeLists.txt

--- Source/App/main.cpp
+++ Source/App/main.cpp
@@ -1,7 +1,15 @@
-#include <iostream>
+#include <QApplication>
+#include <QWidget>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello";
-    return 0;
+    QApplication app(argc, argv);
+    QWidget mainWindow;
+
+    mainWindow.resize(300, 300);
+    mainWindow.setWindowTitle("Media Player");
+
+    mainWindow.show();
+
+    return QApplication::exec();
 }
