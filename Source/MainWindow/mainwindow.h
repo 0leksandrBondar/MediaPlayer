f3ee220077453d517798462d5958ef8609ca5175
b1tfly <thisisandrew0110@gmail.com>
2023-11-25 07:13:56 -1200

    added basic media player

--- Source/MainWindow/mainwindow.h
+++ Source/MainWindow/mainwindow.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QtMultimedia>
+#include <QtCore>
+#include <QtWidgets>
+#include <QString>
+#include <QVideoWidget>
 
 class MainWindow : public QMainWindow
 {
@@ -9,4 +14,6 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(const int  width, const int  height);
     ~MainWindow() = default;
+
+    void PlayVideo(const QString& path);
 };
